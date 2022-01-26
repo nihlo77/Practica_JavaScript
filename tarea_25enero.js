88a7ea6ddb0eccc244dfe19f7180b7e1b9aea446
nihlo <nihlo@outlook.com>
2022-01-25 21:53:40 -0500

    tareas de javascript 25 de enero: positivo o negativo, reglas if y ultimo digito

---
+++ tarea_25enero.js
@@ -0,0 +1,21 @@
+
+let num = 30
+console.log(`\n TAREA 1`)
+if (num == 0) { console.log(`el numero es cero`); }
+else if (num > 0) { console.log(`el número ${num} es positivo`) }
+else { console.log(`el número ${num} es negativo`) }
+
+
+let num1 = 74
+console.log(`\n TAREA 2: el múmero ${num1} es`)
+if (num1 > 5) { console.log(`mayor a 5`) }
+if (num1 == 0) { console.log(`igual a cero`) }
+if (num1 < 5 && num1 > 3) { console.log(`menor a 5 y mayor a 3`) }
+if (num1 > 10 && num1 < 60) { console.log(`mayor a 10 y menor a 3`) }
+if (num1 < 3 && num1 !== 0) { console.log(`memor a 3 y no es 0`) }
+if (num1 < 0) { console.log(`es memor 0`) }
+
+console.log(`\n TAREA 3`)
+let num2 = 5555
+digito = num2 % 10
+console.log(`el último digito de ${num2} es ${digito}`)
